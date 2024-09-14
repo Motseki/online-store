faa670f1110fe2560a0f61c9dce00525535dba6b
Lebohang Motseki <lbmotseki@gmail.com>
2024-09-14 18:37:38 +0200

    Fixing vercel cors errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	crossOrigin: 'anonymous',
+
+	async headers() {
+		return [
+		  {
+			source: '/api/(.*)', // Apply headers to API routes
+			headers: [
+			  {
+				key: 'Access-Control-Allow-Origin',
+				value: 'https://motsekistore.vercel.app'
+			  },
+			  {
+				key: 'Access-Control-Allow-Methods',
+				value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT'
+			  },
+			  {
+				key: 'Access-Control-Allow-Headers',
+				value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+			  },
+			  {
+				key: 'Access-Control-Allow-Credentials',
+				value: 'true'
+			  }
+			]
+		  }
+		]
+	  },
+	// crossOrigin: 'anonymous',
 	images: {
 		domains: ["utfs.io"],
 	}
